401d2c00a652c33a5fcd219e28771d9b44630679
Harsha Gelli <harshagelli765@gmail.com>
2023-08-11 16:31:12 +0530

    Changes

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./Componants/Authentication/Login";
+import Signup from "./Componants/Authentication/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <Signup /> */}
+    </div>
+  );
+}
+
+export default App;
